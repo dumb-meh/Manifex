efc2a3aab2c46391dc7dc7a0fa308c8b26574563
dumb-meh <omorfaruk549@gmail.com>
2025-11-13 15:22:58 +0600

    precision drill and power words features created under presentation

---
+++ app/services/Presentation/precision_drill/precision_drill_route.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, HTTPException, Header,UploadFile, File
+from .precision_drill import PrecisionDrill
+from .precision_drill_schema import PrecisionDrillRequest, PrecisionDrillResponse
+
+
+
+
+
+router = APIRouter()
+precision_drill= PrecisionDrill()     
+
+@router.post("/precision_drill", response_model=PrecisionDrillResponse)
+async def  get_precision_drill(file:UploadFile = File(...),authtoken: str = Header(...)):
+    try:
+        authtoken=verify_token(authtoken)
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid auth token")
+    try:
+        response = precision_drill.generate_precision_drill()
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
