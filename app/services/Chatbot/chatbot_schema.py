15bef070ef3fe3146cc68602f1ca562ad0b0bfc0
dumb-meh <omorfaruk549@gmail.com>
2026-06-04 02:40:20 +0600

    new utils file cache manager added for saving conversation in redis

--- app/services/Chatbot/chatbot_schema.py
+++ app/services/Chatbot/chatbot_schema.py
@@ -1,8 +1,12 @@
 from typing import List, Optional
 from pydantic import BaseModel
 
+class HistoryItem(BaseModel):
+    message: str
+    response: str
+
 class ChatbotMessageRequest:
-    past_conversations: Optional[List[dict]] = None
+    user_id:str
     user_message: str
 
 class ChatbotMessageResponse:
